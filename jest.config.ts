0517bfa2fddbcc37f79629dec79735e8ae0fadbd
Vitor <vitorcoelhof@gmail.com>
2026-02-21 08:22:01 -0300

    fix: correct setupFilesAfterEnv typo in jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const createJestConfig = nextJest({ dir: './' })
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  setupFilesAfterFramework: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 export default createJestConfig(config)
