db26129e8a73646f961c3f9ffbe94bada5bebb0e
Vitor <vitorcoelhof@gmail.com>
2026-02-21 08:45:59 -0300

    feat: add Vitor's hardcoded series list

---
+++ src/data/vitor-series.ts
@@ -0,0 +1,42 @@
+export const VITOR_SERIES = `Breaking Bad
+Prison Break
+Chernobyl
+Game Of Thrones
+Sherlock
+Better Call Saul
+The Office
+Dexter: Ressureição
+Fargo
+Friends
+Succession
+Narcos
+Peaky Blinders
+Ozark
+Dr House
+Dexter
+House of Cards
+Pinguim
+Homeland
+Mr Robot
+Yellowstone
+Stranger Things
+Demolidor
+Suits
+How to get away with murder
+The walking dead
+The handmaid's tale
+Blacklist
+O justiceiro
+Mindhunter
+La Casa de Papel
+O Atirador
+His & Hers
+All her fault
+Billions
+How I Met your mother
+The Last of Us
+The Mentalist
+Lupin
+Senna
+Dexter: Pecado Original
+1923`
